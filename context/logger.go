64d81fa73b9996f5af13e6b3ef0605d3caed8b61
yaxin <yaxin.me@gmail.com>
2025-08-28 11:22:39 +0800

    feat: LoggerWithFields function to inject log fields

--- context/logger.go
+++ context/logger.go
@@ -15,6 +15,12 @@ func WithLogger(ctx stdctx.Context, logger *zap.Logger) stdctx.Context {
 	return stdctx.WithValue(ctx, loggerKey, logger)
 }
 
+func LoggerWithFields(ctx stdctx.Context, fields ...zap.Field) stdctx.Context {
+	l := GetLogger(ctx)
+	nl := l.With(fields...)
+	return WithLogger(ctx, nl)
+}
+
 func GetLogger(ctx stdctx.Context) *zap.Logger {
 	l, ok := ctx.Value(loggerKey).(*zap.Logger)
 	if !ok {
